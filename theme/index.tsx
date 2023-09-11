54dcb6ab84359021449df9b7aeceab54f090d59c
IT - FE(WEB) - Jason <jason.ku@shoalter.com>
2023-09-11 16:44:38 +0800

    update theme 0911

--- theme/index.tsx
+++ theme/index.tsx
@@ -6,19 +6,20 @@ const withTheme = (node: JSX.Element) => (
       <ConfigProvider
         theme={{
           token: {
-            colorPrimary: 'red',
-            colorPrimaryHover: 'red',
-            controlItemBgHover: 'red'
-          },
-          components: {
-            Upload: {
-              colorIcon: 'red',
-              colorIconHover: 'red',
-              colorHighlight: 'red',
-              controlOutline: 'red'
-            }
-          }
-        }}
+            colorTextBase: '#000000',
+            colorText: 'rgba(0, 0, 0, 0.87)',
+            colorPrimary: '#5244ee',
+            colorPrimaryHover: '#7D73F2',
+            colorPrimaryActive: '#1F11BB',
+            colorTextDisabled: 'rgba(0,0,0,0.38)',
+            colorBgContainerDisabled: '#f5f5f5',
+            colorFillSecondary: '#f7f6ff',
+            colorPrimaryBg: '7D73F2',
+            colorError: '#ff4d4f',
+            colorErrorBorderHover: '#ff4d4f',
+            colorErrorOutline: 'rgba(255,77,79,.2)',
+            wireframe: true
+        }}}
       >
         {node}
       </ConfigProvider>
